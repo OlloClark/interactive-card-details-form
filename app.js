f91dc0c19c7336b2247609820869fda49ac596a1
OlloClark <ollo42@hotmail.co.uk>
2022-11-17 14:58:15 -0800

    added src to index.html, adding keyup inputs JS

---
+++ app.js
@@ -0,0 +1,93 @@
+console.log("hello world")
+
+const cardNumber = document.getElementById("card_number");
+const numberInput = document.getElementById("card_number_input");
+
+const cardName = document.getElementById("name");
+const nameInput = document.getElementById("card_name");
+
+const cardMonth = document.getElementById("month");
+const monthInput = document.getElementById("card_month");
+
+const cardYear = document.getElementById("year");
+const yearInput = document.getElementById("card_year");
+
+const cardCvc = document.getElementById("cvc");
+const cvcInput = document.getElementById("card_cvc");
+
+const submitBtn = document.getElementById("submit_btn");
+
+const completed = document.querySelector(".thank")
+const form = document.querySelector("form");
+
+function setCardNumber(e) {
+	cardNumber.innerText = e.target.value;
+}
+
+function setCardName(e) {
+	cardName.innerText = e.target.value;
+}
+
+function setCardMonth(e) {
+	cardMonth.innerText = e.target.value;
+}
+
+function setCardYear(e) {
+	cardYear.innerText = e.target.value;
+}
+
+function setCardCvc(e) {
+	cardCvc.innerText = e.target.value;
+}
+
+function format(s) {
+	return s.toString().replace(/\d{4}(?=.)/g, "$&");
+}
+
+function handleSubmit(e) {
+	e.preventDefault();
+		if (!nameInput.value) {
+			nameInput.classList.add("error");
+			nameInput.parentElement.classList.add("error_message")
+		} else {
+			nameInput.classList.remove("error");
+			nameInput.parentElement.classList.remove("error_message")
+		}
+		if (!numberInput.value) {
+			numberInput.classList.add("error");
+			numberInput.parentElement.classList.add("error_message")
+		} else {
+			numberInput.classList.remove("error");
+			numberInput.parentElement.classList.remove("error_message")
+		}
+		if (!yearInput.value) {
+			yearInput.classList.add("error");
+			yearInput.parentElement.classList.add("error_message")
+		} else {
+			yearInput.classList.remove("error");
+			yearInput.parentElement.classList.remove("error_message")
+		}
+		if (!monthInput.value) {
+			monthInput.classList.add("error");
+			monthInput.parentElement.classList.add("error_message")
+		} else {
+			monthInput.classList.remove("error");
+			monthInput.parentElement.classList.remove("error_message")
+		}
+		if (!cvcInput.value) {
+			cvcInput.classList.add("error");
+			cvcInput.parentElement.classList.add("error_message")
+		} else {
+			cvcInput.classList.remove("error");
+			cvcInput.parentElement.classList.remove("error_message")
+		}
+		console.log("clicked")
+	}
+
+
+numberInput.addEventListener("keyup", setCardNumber);
+nameInput.addEventListener("keyup", setCardName);
+monthInput.addEventListener("keyup", setCardMonth);
+yearInput.addEventListener("keyup", setCardYear);
+cvcInput.addEventListener("keyup", setCardCvc);
+submitBtn.addEventListener("click", handleSubmit);
