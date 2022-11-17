08ff2420438cde5be309d204560268f6675b0164
OlloClark <ollo42@hotmail.co.uk>
2022-11-17 13:52:01 -0800

    getting updated card elements by IDs JS

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const cardNumber = document.getElementById("card_number");
-const numberInput = document.getElementById("card_number_input")
+const numberInput = document.getElementById("card_number_input");
 
-const cardName = document.getElementById("name")
-const numberInput = document.getElementById("card_name")
+const cardName = document.getElementById("name");
+const numberInput = document.getElementById("card_name");
+
+const cardMonth = document.getElementById("month");
+const monthInput = document.getElementById("card_month");
+
+const cardYear = document.getElementById("year");
+const yearInput = document.getElementById("card_year");
+
+const cardCvc = document.getElementById("cvc");
+const cvcInput = document.getElementById("card_cvc");
+
+const submitBtn = document.getElementById("submit_btn");
+
+const form = document.querySelector("form");
