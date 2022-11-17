ef7c0e6fe95864909a4c4f7a25bad8f8514f241f
OlloClark <ollo42@hotmail.co.uk>
2022-11-17 13:51:37 -0800

    getting card elements by IDs JS

---
+++ index.js
@@ -0,0 +1,5 @@
+const cardNumber = document.getElementById("card_number");
+const numberInput = document.getElementById("card_number_input")
+
+const cardName = document.getElementById("name")
+const numberInput = document.getElementById("card_name")
